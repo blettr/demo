10f10e47edd45e457ec4e6005fd46c6a0204d873
lukasbrue <zur@posteo.de>
2022-08-04 15:01:26 +0200

    changed dataset to better format and names

--- data-raw/publictr.R
+++ data-raw/publictr.R
@@ -1,17 +1,27 @@
 library(tidyverse)
 
-df <- read_delim("demo package/demo/data raw/publictr.csv", 
-                  delim = ";", escape_double = FALSE, 
-                  locale = locale(encoding = "WINDOWS-1252"), 
+publictr <- read_delim("data-raw/publictr.csv",
+                  delim = ";", escape_double = FALSE,
+                  locale = locale(encoding = "WINDOWS-1252"),
                   trim_ws = TRUE)
 
-df[df == "..."] <- NA
+publictr[publictr == "..."] <- NA
 
-publictr <- df %>% 
-  select(c(5, 13, 17:20))
+publictr <- publictr %>%
+  select(c(5, 13, 17:20)) %>%
+  select(c(1:3, 5:6, 4))
 
-names(publictr) <- c("Jahr", "Quartal", "Typ", "Anzahl Unternehmen", 
-                     "beförderte Personen [1000]", "Personenkilometer [1000]")
+publictr[ ,2] <- substr(publictr$`2_Auspraegung_Label`, 1, 1)
+
+
+names(publictr) <- c("Jahr", "Quartal", "Typ",
+                     "Personen", "Personenkilometer",
+                     "ausgewertete_Unternehmen")
+
+publictr$Personen <- as.numeric(publictr$Personen)*1000
+publictr$Personenkilometer <- as.numeric(publictr$Personenkilometer)*1000
+publictr$ausgewertete_Unternehmen <- as.numeric(publictr$ausgewertete_Unternehmen)
+publictr$Quartal <- as.numeric(publictr$Quartal)
 publictr
 
 save(publictr, file= "publictr.rda" )
