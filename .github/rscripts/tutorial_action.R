6539512a19e22657773e5b0232e8337f2f1e94da
Lukas Bruelheide <zur@posteo.de>
2024-01-16 18:29:00 +0100

    try gh actions

---
+++ .github/rscripts/tutorial_action.R
@@ -0,0 +1,21 @@
+# Build and install the local package
+devtools::install()
+
+# 1. Install necessary dependencies
+deps <- renv::dependencies("./inst/tutorials")$Package
+installed <- installed.packages()
+to_install <- deps[!(deps %in% installed)]
+lapply(to_install, install.packages)
+
+# 2. Get all possible tutorial paths
+
+tutorial_paths <- list.files("inst/tutorials", recursive = TRUE)
+tutorial_paths <- tutorial_paths[endsWith(tutorial_paths, ".Rmd")]
+tutorial_paths <- tutorial_paths[!is.na(tutorial_paths)]
+tutorial_paths <- paste0("inst/tutorials/", tutorial_paths)
+
+for (p in tutorial_paths) {
+  print(p)
+  try(rmarkdown::render(p))
+}
+
